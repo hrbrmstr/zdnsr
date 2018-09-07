12492182a6fab5fcc74333e1fcc6dc111c5151fc
hrbrmstr <bob@rud.is>
2018-09-07 15:54:51 -0400

    R package repo initialization complete

---
+++ R/zdnsr-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/zdnsr>
+#' - BugReports: <https://gitlab.com/hrbrmstr/zdnsr/issues>
+#' 
+#' @md
+#' @name zdnsr
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
